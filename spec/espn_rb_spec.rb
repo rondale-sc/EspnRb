2ee4b70dd1f85e68b339a8c59ddc948078500e77
Jonathan Jackson <jonj@promedicalinc.com>
2012-03-07 09:18:42 -0500

    Updating spec to reflect change in behavior.

--- spec/espn_rb_spec.rb
+++ spec/espn_rb_spec.rb
@@ -4,28 +4,22 @@ describe EspnRb do
 
   before(:each) do
     @espn = EspnRb.new
-  end
 
-  it "should return 200 response when #all_headlines is called." do
-        stub_request(:get, "http://api.espn.com/sports/news?apikey=#{@espn.api_key}").to_return(
+    # Stub request for valid #all_headlines call
+    stub_request(:get, "http://api.espn.com/sports/news?apikey=#{@espn.api_key}").to_return(
                     :status => 200,
                     :body => File.read('spec/mock_requests/all_headlines_request.txt'),
                     :headers => {})
+  end
 
+  it "should return valid Hash when #all_headlines is called." do
     @espn.all_headlines.class.should eq(Hash)
   end
 
-  it "returns valid espn api resources hash when requested" do
-    @espn.resources.class.should eq(Hash)
-  end
-
-  it "returns valid espn api methods hash when requested" do
-    @espn.methods.class.should eq(Hash)
-  end
+  specify { @espn.resources.class.should eq(Hash) }
+  specify { @espn.methods.class.should eq(Hash) }
 
   it "get_results from api.espn.com" do
-    stub_request(:get, "api.espn.com/sports/news").with(:query => {"apikey" => @espn.api_key})
-
     @espn.get_results(@espn.resources[:professional][:coed][:all][:relative_url], @espn.methods[:news][:relative_url]).class.should eq(Net::HTTPOK)
   end
 end
