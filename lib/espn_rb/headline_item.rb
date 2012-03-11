ecacbd4f509e27089124267784608e827a924e0f
Jonathan Jackson <jonj@promedicalinc.com>
2012-03-11 03:13:17 -0400

    Add methods to Headline Item to get the values of the categories field in the Response.'

--- lib/espn_rb/headline_item.rb
+++ lib/espn_rb/headline_item.rb
@@ -22,6 +22,22 @@ class HeadlineResponse
       @headline["links"]["api"]["news"]["href"]
     end
 
+    def collect_category(sym)
+      @headline["categories"].inject([]) {|m, i| m <<  i[sym.to_s] unless i[sym.to_s].nil?; m}
+    end
+
+    def athletes
+      return collect_category(:description)
+    end
+
+    def leagues
+      return collect_category(:leagueId)
+    end
+
+    def athlete_ids
+      return collect_category(:athleteId)
+    end
+
     def categories
       @headline["categories"]
     end
